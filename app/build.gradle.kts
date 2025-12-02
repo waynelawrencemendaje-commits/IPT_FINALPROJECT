67b505ad876c1196455655817599f974a2534d01
waynelawrencemendaje-commits <waynelawrence.mendaje@hcdc.edu.ph>
2025-12-03 00:54:40 +0800

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     // Add this to enable viewBinding, which is a modern way to access views
     buildFeatures {
